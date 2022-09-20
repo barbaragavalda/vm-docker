80202e4131feb7e71130e504ead326f25e52e634
Bàrbara Gavaldà <barbaragavalda@gmail.com>
2022-09-20 20:44:54 +0200

    Add script to generate tests

---
+++ system/scripts/test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+ORIGINAL_PATH=$(pwd)
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+ROOT_PATH="../.."
+SRC_PATH="${ROOT_PATH}/storage/src"
+TEST_PATH="${ROOT_PATH}/templates/test"
+
+command=$1
+case $command in
+    build-examples)
+        cp -R $TEST_PATH/* $SRC_PATH
+        echo "Test examples created, you may open your browser to check them under localhost"
+    ;;
+    *)
+        echo "Command not found";
+    ;;
+esac
+cd "$ORIGINAL_PATH"
