598bf1a08549ac38e555c405a554df4a531c8b44
Bàrbara Gavaldà <barbaragavalda@gmail.com>
2022-09-20 20:42:41 +0200

    Change variable name and remove unnecessary command

--- system/scripts/docker.sh
+++ system/scripts/docker.sh
@@ -2,17 +2,15 @@
 
 ORIGINAL_PATH=$(pwd)
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-RELATIVE_DOCKERFILE_PATH="../../"
+ROOT_PATH='../..'
 
 command=$1
-
 case $command in
     start)
-        cd $RELATIVE_DOCKERFILE_PATH
+        cd $ROOT_PATH
         sudo docker-compose up --detach --force-recreate --remove-orphans --always-recreate-deps --build
     ;;
     stop)
-        cd $RELATIVE_DOCKERFILE_PATH
         sudo docker-compose down
     ;;
     *)
