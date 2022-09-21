c5a44d28cd8b2f35f8e406120f05e167b80dc708
Bàrbara Gavaldà <barbaragavalda@gmail.com>
2022-09-21 12:00:57 +0200

    Add restart feature in docker script

--- system/scripts/docker.sh
+++ system/scripts/docker.sh
@@ -8,18 +8,31 @@ setEnvironment
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+function start(){
+    cd $ROOT_PATH
+    sudo docker-compose up --detach --force-recreate --remove-orphans --always-recreate-deps --build
+    cdOriginalPath
+}
+
+function stop(){
+    cd $ROOT_PATH
+    sudo docker-compose down
+    cdOriginalPath
+}
+
 command=$1
 case $command in
+    restart)
+        stop
+        start
+    ;;
     start)
-        cd $ROOT_PATH
-        sudo docker-compose up --detach --force-recreate --remove-orphans --always-recreate-deps --build
+        start
     ;;
     stop)
-        sudo docker-compose down
+        stop
     ;;
     *)
         echo "Command not found";
     ;;
 esac
-
-cdOriginalPath
